aa5b18077e6e022488d7118eea5679cf4cdf3fa6
mirwan55 <mirwan.alkhatiri@gmail.com>
2026-06-21 15:44:04 +0700

    Sesi 01: Hello World - program Java pertama

---
+++ 01-fundamentals/Helloworld.java
@@ -0,0 +1,6 @@
+public class Helloworld {
+    static void main() {
+        System.out.println("Hello World!");
+        System.out.println("Java Fullstack Learning Journey dimulai hari ini!");
+    }
+}
