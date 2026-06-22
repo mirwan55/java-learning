bb0c0075f1e212800329013895e0e3a9b8537c81
mirwan55 <mirwan.alkhatiri@gmail.com>
2026-06-22 21:58:46 +0700

    Sesi 02: Variable & Data Type

---
+++ 01-fundamentals/variables/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    static void main(String[] args) {
+        // Tipe data angka bulat
+        int umur = 25;
+        long populasi = 270000000L;
+
+        // Tipe data angka desimal
+        float tinggi = 1.75f;
+        double berat = 68.5;
+
+        // Tipe data karakter & teks
+        char inisial = 'M';
+        String nama = "Andi";
+
+        // Tipe data benar/salah
+        boolean aktif = true;
+
+        // Tampilkan semua
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Aktif: " + aktif);
+    }
+}
